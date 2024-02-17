93c774b2fb75fb17650c1af1e10552b6b6488879
ipopova <innaandreeva17yo@gmail.com>
2024-02-17 17:34:11 +0300

    apply dump

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module sql_proxy
 
-go 1.21.3
+go 1.21
 
 require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	golang.org/dl v0.0.0-20240206215513-68cc34b6ca6f // indirect
 )
--- main.go
+++ main.go
@@ -5,30 +5,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 func getDump(w http.ResponseWriter, r *http.Request) {
-    connStr := "postgres://postgres:root@localhost:5432/postgres?sslmode=disable"
+	connStr := "postgres://postgres:root@localhost:5432/postgres?sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
 		log.Fatal(err)
-	    w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 
 	// age := 21
-	db.Query("CREATE DATABASE users")
+	// db.Query("CREATE DATABASE users")
+
+	file, err := os.ReadFile("../test_dump.sql")
+
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	dump := string(file)
+
+	fmt.Println((dump))
+
+	row, err := db.Query(dump)
+
 	// row, err := db.Query("SELECT name FROM users WHERE age = $1", age)
 
-    // if err != nil {
-	// 	log.Fatal(err)
-	//     w.WriteHeader(http.StatusInternalServerError)
-    // }
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 
-    // fmt.Println(row)
+	fmt.Println(row)
+
+	// fmt.Println(row)
 	w.WriteHeader(http.StatusOK)
 }
 
